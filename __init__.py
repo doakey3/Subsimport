27ca9beb4e5e6113171d64b8b6537a360cd2fb81
doakey3 <github.dmodo@spamgourmet.com>
2016-11-27 11:23:00 -0700

    code cleanup and bug fixes

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@ bl_info = {
     "name": "Subsimport",
     "description": "Import subtitles into blender",
     "author": "doakey3",
-    "version": (1, 1, 0),
+    "version": (1, 1, 1),
     "blender": (2, 7, 8),
     "wiki_url": "https://github.com/doakey3/subsimport",
-    "tracker_url":"https://github.com/doakey3/subsimport/issues",
+    "tracker_url": "https://github.com/doakey3/subsimport/issues",
     "category": "Sequencer"}
 
 import bpy
@@ -13,23 +13,25 @@ from .import_subtitle import ImportSubtitle
 from .shortcut_functions import ShiftFrameStart
 from .shortcut_functions import ShiftFrameEnd
 from .shortcut_functions import ShiftBoth
-    
+
+
 class subsimport_UI(bpy.types.Panel):
     bl_space_type = "SEQUENCE_EDITOR"
     bl_region_type = "UI"
     bl_label = "Subsimport"
-    
+
     def draw(self, context):
         layout = self.layout
         row = layout.row()
-        row.prop(context.scene,'subtitle_font_size',
-            text='Subtitle Font Size')
+        row.prop(context.scene, 'subtitle_font_size',
+                 text='Subtitle Font Size')
         row = layout.row()
-        row.operator('sequencerextra.import_subtitle',icon="TEXT")
-            
+        row.operator('sequencerextra.import_subtitle', icon="TEXT")
+
+
 def register():
     bpy.utils.register_module(__name__)
-    
+
     bpy.types.Scene.subtitle_channel = bpy.props.IntProperty(
         description="The channel where subtitles will be added",
         default=3,
@@ -38,18 +40,21 @@ def register():
         description="The font_size of the added text strips after import",
         default=70,
         min=1)
-    
+
     kc = bpy.context.window_manager.keyconfigs.addon
     km = kc.keymaps.new(name="Sequencer", space_type="SEQUENCE_EDITOR")
-    kmi = km.keymap_items.new("sequencerextra.shift_text_frame_start", "D", 'PRESS')
-    kmi = km.keymap_items.new("sequencerextra.shift_text_frame_end", "F", 'PRESS')
+    kmi = km.keymap_items.new("sequencerextra.shift_text_frame_start",
+                              "D", 'PRESS')
+    kmi = km.keymap_items.new("sequencerextra.shift_text_frame_end",
+                              "F", 'PRESS')
     kmi = km.keymap_items.new("sequencerextra.shift_both", "S", 'PRESS')
-    
+
+
 def unregister():
     del bpy.types.Scene.subtitle_channel
-    
+
     kc = bpy.context.window_manager.keyconfigs.addon
-    km = kc.keymaps["Sequencer"] 
+    km = kc.keymaps["Sequencer"]
     for kmi in km.keymap_items:
         if kmi.idname in ["sequencerextra.shift_text_frame_start",
                           "sequencerextra.shift_text_frame_end",
--- shortcut_functions.py
+++ shortcut_functions.py
@@ -1,87 +1,91 @@
 import bpy
 
+
 class ShiftFrameStart(bpy.types.Operator):
     bl_idname = "sequencerextra.shift_text_frame_start"
     bl_label = "Shift Frame Start of Next Text Strip"
-    bl_description = "Shifts the frame start of text strip to the current frame"
-    
+    bl_description = "Shifts frame start of text strip to the current frame"
+
     def execute(self, context):
         scene = context.scene
         current_frame = scene.frame_current
         seq = bpy.context.scene.sequence_editor
-        if not seq == None:
+        if seq:
             all_strips = list(sorted(seq.sequences_all,
-                key=lambda x: x.frame_start))
+                                     key=lambda x: x.frame_start))
             text_strips = []
             for strip in all_strips:
                 if strip.type == "TEXT":
                     text_strips.append(strip)
             if not len(text_strips) == 0:
                 for i in range(len(text_strips)):
-                    if (text_strips[i].frame_final_start < current_frame and 
-                    text_strips[i].frame_final_end > current_frame):
+                    if (text_strips[i].frame_final_start < current_frame and
+                            text_strips[i].frame_final_end > current_frame):
                         text_strips[i].frame_final_start = current_frame
                         return {"FINISHED"}
-                    elif (text_strips[i].frame_final_start >= current_frame and 
-                    text_strips[i].frame_final_end > current_frame and
-                    i < len(text_strips)):
+                    elif (text_strips[i].frame_final_start >= current_frame and
+                            text_strips[i].frame_final_end > current_frame and
+                            i < len(text_strips)):
                         text_strips[i].frame_final_start = current_frame
                         return {"FINISHED"}
-                        
+
             else:
                 return {"FINISHED"}
         else:
             return {"FINISHED"}
-            
+
         return {"FINISHED"}
+
 
 class ShiftFrameEnd(bpy.types.Operator):
     bl_idname = "sequencerextra.shift_text_frame_end"
     bl_label = "Shift Frame End of Next Text Strip"
     bl_description = "Shifts the frame end of text strip to the current frame"
-    
+
     def execute(self, context):
         scene = context.scene
         current_frame = scene.frame_current
         seq = bpy.context.scene.sequence_editor
-        if not seq == None:
+        if seq:
             all_strips = list(sorted(seq.sequences_all,
-                key=lambda x: x.frame_start))
+                                     key=lambda x: x.frame_start))
             text_strips = []
             for strip in all_strips:
                 if strip.type == "TEXT":
                     text_strips.append(strip)
             if not len(text_strips) == 0:
                 for i in range(len(text_strips)):
-                    if i == 0 and text_strips[i].frame_final_start > current_frame:
+                    if (i == 0 and
+                            text_strips[i].frame_final_start > current_frame):
                         return {"FINISHED"}
-                    elif (text_strips[i].frame_final_start < current_frame and 
-                    text_strips[i].frame_final_end > current_frame):
+                    elif (text_strips[i].frame_final_start < current_frame and
+                            text_strips[i].frame_final_end > current_frame):
                         text_strips[i].frame_final_end = current_frame
                         return {"FINISHED"}
-                    elif (text_strips[i].frame_final_start >= current_frame and 
-                    text_strips[i].frame_final_end > current_frame):
+                    elif (text_strips[i].frame_final_start >= current_frame and
+                            text_strips[i].frame_final_end > current_frame):
                         text_strips[i-1].frame_final_end = current_frame
                         return {"FINISHED"}
                     elif i == len(text_strips) - 1:
                         text_strips[i].frame_final_end = current_frame
                         return {"FINISHED"}
-                        
+
                 return {"FINISHED"}
             else:
                 return {"FINISHED"}
         else:
             return {"FINISHED"}
-            
+
         return {"FINISHED"}
+
 
 class ShiftBoth(bpy.types.Operator):
     bl_idname = "sequencerextra.shift_both"
     bl_label = "Shift Frame End then Frame start of next"
     bl_description = "A combo of the D and F key shortcuts in a single key"
-    
+
     def execute(self, context):
         bpy.ops.sequencerextra.shift_text_frame_end()
         bpy.ops.sequencerextra.shift_text_frame_start()
-        
+
         return {"FINISHED"}
